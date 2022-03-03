e74d2a3567c78fe492a760578c21d7cde05368fe
iamapell <exhthx@mail.ru>
2022-03-03 21:39:12 +0700

    Подготовка серверной части

--- web/main/views.py
+++ web/main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def get_some_information(request):
+    info = {
+        'one': 1,
+        'two': 'четыре',
+        'obama': ['obama', 'obama']
+    }
+    return JsonResponse(info)
