778b94ccac433e7e6a2e18e4c5371f7e27ce8406
dahhwe <68936213+dahhwe@users.noreply.github.com>
2022-03-08 00:17:10 +0700

    Update models.py
    Созданы Django-модели объектов

--- web/main/models.py
+++ web/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField('Имя студента', max_length=30)
+    surname = models.CharField('Фамилия студента', max_length=60)
+    last_name = models.CharField('Отчество студента', max_length=60)
+    group = models.CharField('Группа студента', max_length=20)
+    form_of_education = models.CharField('Форма обучения', max_length=40)
+    vk_link = models.CharField('Ссылка на VK студента', max_length=300)
+    github_link = models.CharField('Ссылка на GitHib студента', max_length=300)
+
+
+class Project(models.Model):
+    name = models.CharField('Название проекта', max_length=30)
+    github_link = models.CharField('Ссылка на GitHib проекта', max_length=300)
+    check_statistic = models.FloatField('Статистика с момента последней проверки')
+    new_commits_num = models.PositiveIntegerField('Количество новых коммитов')
+    open_tasks_num = models.PositiveIntegerField('Количество открытых задач')
+    tasks_in_progress_num = models.PositiveIntegerField('Количество задач в работе')
+    closed_tasks_num = models.PositiveIntegerField('Количество закрытых задач')
+    last_release_num = models.PositiveIntegerField('Номер последнего релиза', default='')
+    rating = models.DecimalField('Рейтинг проекта', decimal_places=2, max_digits=2)
+    week_lines_progress = models.IntegerField('Количество удалений и добавлений строк за неделю')
+
+
+class Groups(models.Model):
+    studying_year = models.DateField('Учебный год')
+    all = models.TextField('Все команды этого года')
+
+class User(models.Model):
+    name = models.CharField('Имя пользователя', max_length=30)
+    surname = models.CharField('Фамилия пользователя', max_length=30)
