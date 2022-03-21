d7a3767ed33625411eae55548fbd99d892cdf583
dahhwe <68936213+dahhwe@users.noreply.github.com>
2022-03-22 00:15:11 +0700

    Обновлены Django Модели

--- web/main/models.py
+++ web/main/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
 
-class Student(models.Model):
-    name = models.CharField('Имя студента', max_length=30)
-    surname = models.CharField('Фамилия студента', max_length=60)
-    last_name = models.CharField('Отчество студента', max_length=60)
-    group = models.CharField('Группа студента', max_length=20)
-    form_of_education = models.CharField('Форма обучения', max_length=40)
-    vk_link = models.CharField('Ссылка на VK студента', max_length=300)
-    github_link = models.CharField('Ссылка на GitHib студента', max_length=300)
-
-
 class Project(models.Model):
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
     name = models.CharField('Название проекта', max_length=30)
     github_link = models.CharField('Ссылка на GitHib проекта', max_length=300)
     check_statistic = models.FloatField('Статистика с момента последней проверки')
@@ -23,11 +18,39 @@ class Project(models.Model):
     rating = models.DecimalField('Рейтинг проекта', decimal_places=2, max_digits=2)
     week_lines_progress = models.IntegerField('Количество удалений и добавлений строк за неделю')
 
+    def __str__(self):
+        return self.name
 
-class Groups(models.Model):
-    studying_year = models.DateField('Учебный год')
-    all = models.TextField('Все команды этого года')
 
-class User(models.Model):
-    name = models.CharField('Имя пользователя', max_length=30)
-    surname = models.CharField('Фамилия пользователя', max_length=30)
+class Student(models.Model):
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    name = models.CharField('Имя студента', max_length=30)
+    surname = models.CharField('Фамилия студента', max_length=60)
+    patronymic = models.CharField('Отчество студента', max_length=60)
+    is_team_lead = models.BooleanField(default=False)
+    group = models.CharField('Группа студента', max_length=20)
+    form_of_education = models.CharField('Форма обучения', max_length=40)
+    vk_link = models.CharField('Ссылка на VK студента', max_length=300)
+    github_link = models.CharField('Ссылка на GitHib студента', max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class YearGroup(models.Model):
+
+    class Meta:
+        verbose_name = 'Поток'
+        verbose_name_plural = 'Потоки'
+
+    name = models.CharField('Учебный год', max_length=30)
+    studying_year = models.IntegerField('Учебный год')
+    is_main = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
