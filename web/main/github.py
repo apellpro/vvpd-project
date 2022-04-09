1f0fbae391225c60b3d70615967465906b630e08
FR4G-TP <pdaniil080503@gmail.com>
2022-04-10 01:00:21 +0700

    Обновление Github API

--- web/main/github.py
+++ web/main/github.py
@@ -1,39 +1,54 @@
 from requests import request
 
-def getUsersCommits(repo: str):
-    payload={}
-    headers = {}
-    return len(request("GET", "https://api.github.com/repos/" + repo + "commits", headers=headers, data=payload).json())
 
-def getCommitBySHA(repo, sha: str):
-    payload={}
+def get_users_commits(owner, repo: str):
+    payload = {}
     headers = {}
-    return request("GET", "https://api.github.com/repos/" + repo + "commits/" + sha, headers=headers, data=payload).json()
+    commits = len(request("GET", f"https://api.github.com/repos/{owner}/{repo}/commits", headers=headers, data=payload).json())
 
-def getCommitActivity(repo: str):
-    payload={}
-    headers = {}
-    return request("GET", "https://api.github.com/repos/" + repo + "stats/commit_activity" , headers=headers, data=payload).json()
 
-def getMetrics(repo: str):
-    payload={}
+def get_commit_by_sha(owner, repo, sha: str):
+    payload = {}
     headers = {}
-    return request("GET", "https://api.github.com/repos/" + repo + "community/profile", headers=headers, data=payload).json()
+    return request("GET", f"https://api.github.com/repos/{owner}/{repo}/commits/{sha}", headers=headers, data=payload).json()
 
-def getIssuesNum(repo: str):
-    payload={}
+
+def get_commit_activity(owner, repo: str):
+    payload = {}
     headers = {}
-    issues_num = (request("GET", "https://api.github.com/" + "search/issues?q=repo:" + repo, headers=headers, data=payload).json())["total_count"]
-    open_issues_num = (request("GET", "https://api.github.com/" + "search/issues?q=repo:" + repo + "+state:open", headers=headers, data=payload).json())["total_count"]
+    return request("GET", f"https://api.github.com/repos/{owner}/{repo}/stats/commit_activity", headers=headers, data=payload).json()
+
+
+def get_metrics(owner, repo: str):
+    payload = {}
+    headers = {}
+    return request("GET", f"https://api.github.com/repos/{owner}/{repo}/community/profile", headers=headers, data=payload).json()
+
+
+def get_issues_num(owner, repo: str):
+    payload = {}
+    headers = {}
+    issues = request("GET", f"https://api.github.com/search/issues?q=repo:{owner}/{repo}", headers=headers, data=payload).json()
+    issues_num = issues["total_count"]
+    open_issues_num = 0
+    for i in issues:
+        if i["state"] == "open":
+            open_issues_num += 1
     return issues_num, open_issues_num
 
-def getOpenedIssues(repo: str):
-    payload={}
+
+def get_opened_issues(owner, repo: str):
+    payload = {}
     headers = {}
-    issues = request("GET", "https://api.github.com/" + "search/issues?q=repo:" + repo + "+state:open", headers=headers, data=payload).json()
+    issues = request("GET", f"https://api.github.com/search/issues?q=repo:{owner}/{repo}+state:open", headers=headers, data=payload).json()
     return issues
 
-def getReleases(repo: str):
-    payload={}
+
+def get_releases(owner, repo: str):
+    payload = {}
     headers = {}
-    return request("GET", "https://api.github.com/repos/" + repo + "releases", headers=headers, data=payload).json()
+    imported_releases = request("GET", f"https://api.github.com/repos/{owner}/{repo}/releases", headers=headers, data=payload).json()
+    releases = dict()
+    for i in imported_releases:
+        releases[i["name"]] = i["created_at"]
+    return releases
